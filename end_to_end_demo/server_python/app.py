ab8876110fa472cb548c96e781ba29c84ce9d820
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2022-08-18 17:05:53 +1000

    update to python 0.0.4

--- end_to_end_demo/server_python/app.py
+++ end_to_end_demo/server_python/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
+import grpc
 from userwatch import userwatch
 from userwatch import userwatch_public_pb2
 import getopt
@@ -24,37 +25,47 @@ def register():
     username = jsonRequest["username"]
     userwatchToken = jsonRequest["userwatchToken"]
 
-    validateResponse = userwatchClient.validate(
-        userwatchToken,
-        userwatch_public_pb2.UserInfo(user_name=username),
-        userwatch_public_pb2.EVENT_TYPE_REGISTER
-    )
-
     status = 200
-    if validateResponse.action == userwatch_public_pb2.OUTCOME_DENY:
-        print("deny returning 403")
-        status = 403
-    if validateResponse.action == userwatch_public_pb2.OUTCOME_CHALLENGE:
-        print("challenge returning 401")
-        status = 401
+
+    try:
+        validateResponse = userwatchClient.verify(
+            userwatchToken,
+            userwatch_public_pb2.UserInfo(user_name=username)
+        )
+        if validateResponse.action == userwatch_public_pb2.OUTCOME_DENY:
+            print("deny returning 403")
+            status = 403
+        if validateResponse.action == userwatch_public_pb2.OUTCOME_CHALLENGE:
+            print("challenge returning 401")
+            status = 401
+
+    except grpc.RpcError as err:
+        if err and err.code() == grpc.StatusCode.INVALID_ARGUMENT:
+            print("Token was invalid, could be a malicious request, challenge 401")
+            status = 401
+        else:
+            print("Other errors suggest a system issue. Give benefit of the doubt. 200")
+            status = 200
 
     return Response(status=status, response="{}", content_type="application/json")
+
 
 def usage():
     print('app.py --port <port>')
 
+
 if __name__ == "__main__":
-    port=5000
+    port = 5000
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hp:", ["port="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
     for opt, arg in opts:
-      if opt == '-h':
-         usage()
-         sys.exit()
-      elif opt in ("-p", "--port"):
-         port = arg
-    
+        if opt == '-h':
+            usage()
+            sys.exit()
+        elif opt in ("-p", "--port"):
+            port = arg
+
     app.run(port=port)
--- end_to_end_demo/server_python/readme.md
+++ end_to_end_demo/server_python/readme.md
@@ -12,7 +12,7 @@ source venv/bin/activate
 python3 -m pip install -r requirements.txt
 
 # optional: If you need to install a new test version of userwatch-python from test pypi.
-python3 -m pip install -i https://test.pypi.org/simple/ userwatch-python==0.0.5
+python3 -m pip install -i https://test.pypi.org/simple/ userwatch-python==0.0.4
 
 # run dev server
 python3 app.py --port 8001
